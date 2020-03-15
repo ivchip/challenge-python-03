72b1e4b210b64c563440463df59cac65e71cef1e
Iván Chipatecua <ivchip@gmail.com>
2020-03-14 19:14:14 -0500

    Challenge is completed

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
 # Resolve the problem!!
+import re
 
 
 def run():
     # Start coding here
+    with open('encoded.txt', 'r', encoding='utf-8') as f:
+        text = f.read()
+
+    hide_word = re.findall(r'[a-z]', text)
+    print(''.join(hide_word))
 
 
 if __name__ == '__main__':
